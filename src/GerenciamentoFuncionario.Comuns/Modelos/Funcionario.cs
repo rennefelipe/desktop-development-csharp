3d8c42ec32a616cd8b616f1159ce0e91b860d5ee
rennefelipe <rennefelipe@outlook.com.br>
2022-08-12 22:20:44 -0300

    segundo comit

---
+++ src/GerenciamentoFuncionario.Comuns/Modelos/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoFuncionario.Comuns.Modelos
+{
+    class Funcionario
+    {
+        public int id { get; protected set; }
+        private string _nomeCompleto;
+
+        public string NomeCompleto { get; protected set; }
+
+        public int CargoId { get; protected set; }
+
+        public bool EBebedorCafe { get; protected set; }
+
+        public String PrimeiroNome { get; protected set; }
+
+        public string UltimoNome { get; protected set; }
+
+        public DateTimeOffset DataEntrada { get; protected set; }
+
+
+
+
+    }
+}
